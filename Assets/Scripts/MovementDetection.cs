73930d02d86aaccd9b6e59109d9e552fedec5184
Allaiza <80018510+Allaiza@users.noreply.github.com>
2022-03-21 19:02:30 +0800

    Add files via upload

---
+++ Assets/Scripts/MovementDetection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementDetection : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = Input.acceleration;
+
+        Debug.Log(dir.ToString());
+    }
+}
